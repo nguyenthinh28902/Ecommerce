269c7b1673403c9fbdf81f7b2e4db816dce8aa40
ThinhNguyen2692 <nndthinh2802@gmail.com>
2023-07-01 23:24:45 +0700

    first commit

---
+++ ModelProject/EcommerceSetting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelEntity
+{
+    public static class EcommerceSetting
+    {
+        public static string EcommerceConnectionString { get; set;}
+
+    }
+}
