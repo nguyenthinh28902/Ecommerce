3dd74c00fe04ce952732c561b1dfeb514ba6f3c8
nguyenthinh28902 <nndthinh28902@gmail.com>
2024-09-27 15:55:39 +0700

    add project apigateway.user

---
+++ ApiGateway.User/Helpers/OcelotSetting.cs
@@ -0,0 +1,7 @@
+﻿namespace ApiGateway.User.Helpers
+{
+    public class OcelotSetting
+    {
+        public List<string> OcelotNames { get; set; } = new List<string>();
+    }
+}
---
+++ ApiGateway.User/Register/RegisterOcelot.cs
@@ -0,0 +1,25 @@
+﻿using ApiGateway.User.Helpers;
+
+namespace ApiGateway.User.Register
+{
+    public static class RegisterOcelot
+    {
+        public static IServiceCollection ServiceRegisterOcelot(this IServiceCollection services, ConfigurationManager configurationManager)
+        {
+            var ocelotSettings = new OcelotSetting();
+            configurationManager.GetSection("OcelotSetting").Bind(ocelotSettings);
+            if (ocelotSettings != null)
+            {
+                var currentDirectory = Environment.CurrentDirectory;
+                foreach (var item in ocelotSettings.OcelotNames)
+                {
+
+                    var filePath = Path.Combine(currentDirectory, "OcelotJson", item);
+
+                    configurationManager.AddJsonFile(filePath, optional: false, reloadOnChange: true);
+                }
+            }
+            return services;
+        }
+    }
+}
